b0c753eaffdd5d686fc75714ae8b4d9750e0798b
Cesar Casares <cacjmuse@gmamil.com>
2023-10-05 22:43:05 -0500

    feat: "configured custom routes of pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   def home
     @posts = Blog.all
     @skills = Skill.all
-    @portfolios = Porfolio.all
+    @portfolios = Portfolio.all
   end
 
   def about
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   resources :portfolios
   resources :blogs, :skills
-  get 'pages/home'
-  get 'pages/about'
-  get 'pages/contact'
-  get 'hello/world'
+  # get 'pages/home'
+  get 'about-me', to: 'pages#about'
+  get 'contact', to: 'pages#contact'
+  # get 'hello/world'
+
+  root to: 'pages#home'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
