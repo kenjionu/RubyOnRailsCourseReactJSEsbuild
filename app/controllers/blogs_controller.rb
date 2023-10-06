d8a2a3bb280a15ce0812996c9ce75f5333e34efc
Cesar Casares <cacjmuse@gmamil.com>
2023-10-06 00:30:50 -0500

    feat: integrated friendly_id gem routes for blog posts

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -8,6 +8,8 @@ class BlogsController < ApplicationController
 
   # GET /blogs/1 or /blogs/1.json
   def show
+    @blogs = Blog.friendly.find(params[:id])
+
   end
 
   # GET /blogs/new
@@ -60,7 +62,7 @@ class BlogsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_blog
-      @blog = Blog.find(params[:id])
+      @blog = Blog.friendly.find(params[:id])
     end
 
     # Only allow a list of trusted parameters through.
