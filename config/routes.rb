20284d14c56e54e8387910997a78ef2bed5243be
Cesar Casares <cacjmuse@gmamil.com>
2023-10-10 21:26:35 -0500

    feat: updated status

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
   resources :portfolios
-  resources :blogs, :skills
+  resources :skills
   # get 'pages/home'
   get 'porftfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
   get 'about-me', to: 'pages#about'
   get 'contact', to: 'pages#contact'
+  resources :blogs do
+    member do
+      get :toggle_status
+    end
+  end
   # get 'hello/world'
 
   root to: 'pages#home'
