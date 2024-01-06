8547f383f08e04a226ed5c4aed9a1279d56c385c
Cesar Casares <cacjmuse@gmamil.com>
2024-01-06 01:01:15 -0500

    Upgrade Rails 7.0.8 TO 7.1 Added custom layouts for blogs and portfolios

--- config/puma.rb
+++ config/puma.rb
@@ -11,6 +11,14 @@ threads min_threads_count, max_threads_count
 # Specifies the `worker_timeout` threshold that Puma will use to wait before
 # terminating a worker in development environments.
 #
+# Specifies that the worker count should equal the number of processors in production.
+if ENV["RAILS_ENV"] == "production"
+  require "concurrent-ruby"
+  worker_count = Integer(ENV.fetch("WEB_CONCURRENCY") { Concurrent.physical_processor_count })
+  workers worker_count if worker_count > 1
+end
+
+
 worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
