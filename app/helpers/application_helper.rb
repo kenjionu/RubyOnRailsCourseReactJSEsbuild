7c88f173c5f59fa776ba88c25d396a5413a71a11
Cesar Casares <cacjmuse@gmamil.com>
2024-01-15 19:26:12 -0500

    Implemented Authorization engine for portfolio items and blogs

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,11 @@
 module ApplicationHelper
     def login_helper
-    if current_user.is_a?(User) 
-      link_to "Logout", destroy_user_session_path, class: "text-sm font-semibold leading-6 text-gray-900", data: { turbo_method: :delete } 
-    else 
+    if current_user.is_a?(GuestUser) 
       (link_to "Register", new_user_registration_path, method: :get,  class: "text-sm font-semibold leading-6 text-gray-900") +
-        "<br>".html_safe + 
+      "<br>".html_safe + 
       (link_to "Login", new_user_session_path, method: :get,  class: "text-sm font-semibold leading-6 text-gray-900") 
+    else 
+      link_to "Logout", destroy_user_session_path, class: "text-sm font-semibold leading-6 text-gray-900", data: { turbo_method: :delete } 
      end 
     end
     def source_helper(layout_name)
