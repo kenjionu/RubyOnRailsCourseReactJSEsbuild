28311432de08b2db05d5e01cf195d94b3fa1d10a
Cesar Casares <cacjmuse@gmamil.com>
2024-01-29 21:43:22 -0500

    Updated styles for master layout file

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,7 @@
 module ApplicationHelper
     def login_helper
     if current_user.is_a?(GuestUser) 
-      (link_to "Register", new_user_registration_path, method: :get,  class: "text-sm font-semibold leading-6 text-gray-900") +
-      "<br>".html_safe + 
+      (link_to "Register", new_user_registration_path, method: :get,  class: "text-sm font-semibold leading-6 text-gray-900").html_safe + 
       (link_to "Login", new_user_session_path, method: :get,  class: "text-sm font-semibold leading-6 text-gray-900") 
     else 
       link_to "Logout", destroy_user_session_path, class: "text-sm font-semibold leading-6 text-gray-900", data: { turbo_method: :delete } 
